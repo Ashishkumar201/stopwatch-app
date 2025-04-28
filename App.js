ca0854398c0d8a9bf1f787ea39a04d1e87a74998
Ashishkumar201 <ashishkumarbagria@gmail.com>
2025-04-28 17:16:45 +0530

    My first React Native app published

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+// import Stopwatch from './components/Stopwatch';
+import Stopwatch from './Stopwatch';
 export default function App() {
   return (
+    <div>
+    <div><Stopwatch/></div>
+    <div>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </div>
+    </div>
   );
 }
 
